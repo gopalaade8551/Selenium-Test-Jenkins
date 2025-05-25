fa48e03b152c39b67a88429702def02870ef4219
Gopal Ade <gopalade8551@gmail.com>
2025-05-25 23:38:45 +0530

    git parameter added

--- src/test/java/Jenkins_01/Orange_Title.java
+++ src/test/java/Jenkins_01/Orange_Title.java
@@ -4,7 +4,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,11 +14,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Orange_Title {
 
+	
+	@Parameters("Browser")
 	@Test
-	public void startbrowser() {
+	public void startbrowser(String browsername) {
+		
+		System.out.println("the parameter value is "+browsername);
+		WebDriver driver=null;
+		if(browsername.contains("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+		}
+		
+		else if(browsername.contains("Edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		 driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
